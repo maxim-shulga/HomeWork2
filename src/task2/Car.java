6844c90ee44118f4ceb06f514d87a677f4b845f6
maxim <maxim.shulga.p@gmail.com>
2023-03-07 17:37:15 +0200

    Blank project is created

---
+++ src/task2/Car.java
@@ -0,0 +1,17 @@
+package task2;
+
+public class Car {
+    private int year;
+    private String color;
+    public Car(){
+        this(2022, "grey");
+    }
+    public Car(int year){
+        this(year, "grey");
+    }
+    public Car(int year, String color){
+        this.year = year;
+        this.color = color;
+    }
+
+}
