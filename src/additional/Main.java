a4d7c8006e7c40384fb500db2a0f6019a6fc01c8
maxim <maxim.shulga.p@gmail.com>
2023-03-07 18:27:00 +0200

    additional task is added

---
+++ src/additional/Main.java
@@ -0,0 +1,8 @@
+package additional;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(MyArea.areaOfCircle(10));
+    }
+}
---
+++ src/additional/MyArea.java
@@ -0,0 +1,8 @@
+package additional;
+
+public class MyArea {
+    static final double PI = 3.14;
+    public static double areaOfCircle(int radius){
+        return PI*radius;
+    }
+}
